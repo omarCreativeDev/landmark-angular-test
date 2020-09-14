96b3584eefdda1ce5438ff8590ecef16887e1259
Omar Mirza <omar.creative.dev@gmail.com>
2020-09-14 20:10:27 +0100

    wip edit price

---
+++ src/app/product-detail/edit-price/edit-price.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-price',
+  templateUrl: './edit-price.component.html',
+})
+export class EditPriceComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/product-detail/product-detail.module.ts
+++ src/app/product-detail/product-detail.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProductDetailRoutingModule } from './product-detail-routing.module';
 import { ProductDetailComponent } from './product-detail.component';
 import { SharedModule } from '../shared/shared.module';
+import { EditPriceComponent } from './edit-price/edit-price.component';
 
 @NgModule({
-  declarations: [ProductDetailComponent],
+  declarations: [ProductDetailComponent, EditPriceComponent],
   imports: [CommonModule, ProductDetailRoutingModule, SharedModule],
 })
 export class ProductDetailModule {}
