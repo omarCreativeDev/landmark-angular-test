d03f28dfb1a474b8c75b293bdebf8673d7e7d09c
Omar Mirza <omar.creative.dev@gmail.com>
2020-09-14 15:12:05 +0100

    leverage async pipe and update service binding due to rename

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { ListService } from '../services/list.service';
 import { delay, finalize, take } from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs';
 import { Product } from '../interfaces';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
 })
 export class ListComponent implements OnInit {
-  public loading = true;
+  public loading$: Observable<boolean> = this.productsService.loading$;
+  public products$: Observable<Product[]> = this.productsService.products$;
   public displayedColumns: string[] = ['name', 'stock', 'updated', 'price'];
-  public products$: Observable<Product[]> = this.listService.products$;
 
-  constructor(private listService: ListService) {}
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
     this.listProducts();
   }
 
   public listProducts(): Subscription {
-    return this.listService
+    return this.productsService
       .listProducts()
       .pipe(
         take(1),
         delay(1000),
-        finalize(() => (this.loading = false))
+        finalize(() => this.productsService.loading$.next(false))
       )
-      .subscribe((response) => this.listService.products$.next(response));
+      .subscribe((response: Product[]) =>
+        this.productsService.products$.next(response)
+      );
   }
 }
