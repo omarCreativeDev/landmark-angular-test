e22d89f4e4a7ba3ba8f74daed0f4e65d9e04ef82
Omar Mirza <omar.creative.dev@gmail.com>
2020-09-15 08:26:30 +0100

    setup upload form and component and layout

--- src/app/upload-products/upload-form/upload-form.component.ts
+++ src/app/upload-products/upload-form/upload-form.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html',
+  styleUrls: ['./upload-form.component.scss'],
 })
 export class UploadFormComponent implements OnInit {
-  constructor() {}
+  public form: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.setupForm();
+  }
+
+  public checkForm(): void {
+    if (this.form.valid) {
+      this.uploadProducts();
+    }
+  }
+
+  public setupForm(): void {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      stock: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+    });
+  }
+
+  public uploadProducts(): void {}
 }
--- src/app/upload-products/upload-products.module.ts
+++ src/app/upload-products/upload-products.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UploadFormComponent } from './upload-form/upload-form.component';
 import { UploadProductsRoutingModule } from './upload-products-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UploadFormComponent],
-  imports: [CommonModule, UploadProductsRoutingModule],
+  imports: [CommonModule, UploadProductsRoutingModule, SharedModule],
 })
 export class UploadProductsModule {}
